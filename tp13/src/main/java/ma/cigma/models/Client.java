e46d262ba76f9707e5f825f297a8c3fb80b736dd
MATARREDA <96024279+MATARREDA@users.noreply.github.com>
2022-03-14 21:05:16 +0000

    TP13
    TP13

---
+++ tp13/src/main/java/ma/cigma/models/Client.java
@@ -0,0 +1,12 @@
+package ma.cigma.models;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+public class Client {
+	private long id;
+	private String name;
+
+
+}
---
+++ tp13/src/main/java/ma/cigma/models/package-info.java
@@ -0,0 +1,1 @@
+package ma.cigma.models;
