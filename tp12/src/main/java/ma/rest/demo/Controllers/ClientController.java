bb5e6df6b948897265ac8b0b3784a5b1349028ec
MATARREDA <96024279+MATARREDA@users.noreply.github.com>
2022-03-30 23:30:49 +0000

    Add files via upload
    tp12 v2

--- tp12/src/main/java/ma/rest/demo/Controllers/ClientController.java
+++ tp12/src/main/java/ma/rest/demo/Controllers/ClientController.java
@@ -2,7 +2,6 @@ package ma.rest.demo.Controllers;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import ma.rest.demo.Models.Client;
 import ma.rest.demo.Service.IService;
 
--- tp12/src/main/java/ma/rest/demo/MockClients.java
+++ tp12/src/main/java/ma/rest/demo/MockClients.java
@@ -15,22 +15,23 @@ public class MockClients implements CommandLineRunner {
 	@Autowired
 	private IClientDAO clientRepository;
 
-
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-				Client c1 = new Client("Omar");
-				Client c2 = new Client("Said");
-				Client c3 = new Client("Ahmed");
-				Client c4 = new Client("Farah");
-				clientRepository.save(c1);
-				clientRepository.save(c2);
-				clientRepository.save(c3);
-				clientRepository.save(c4);
-				clientRepository.findAll().forEach(c ->
-				System.out.println
-				(c)
-				);
+//				Client c1 = new Client("Omar");
+//				Client c2 = new Client("Said");
+//				Client c3 = new Client("Ahmed");
+//				Client c4 = new Client("Farah");
+//				clientRepository.save(c1);
+//				clientRepository.save(c2);
+//				clientRepository.save(c3);
+//				clientRepository.save(c4);
+//				clientRepository.findAll().forEach(c ->System.out.println(c));
+	}
+
+
+	public static Logger getLogger() {
+		return logger;
 	}
 
 }
